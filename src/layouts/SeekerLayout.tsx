32819d70c4af9e51147ad73591a97bc3e6c706be
Isreal5161 <diatecktechnology@gmail.com>
2026-09-16 20:48:59 +0100

    update cta button

--- src/layouts/SeekerLayout.tsx
+++ src/layouts/SeekerLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import DashboardTopbar from '../components/layout/DashboardTopbar';
 import MobileBottomNav from '../components/layout/MobileBottomNav';
@@ -6,9 +6,11 @@ import Footer from '../components/layout/Footer';
 
 function SeekerLayout() {
   const { user, logout } = useAuth();
+  const { pathname } = useLocation();
+  const isJobDetailPage = pathname.startsWith('/seeker/jobs/');
 
   return (
-    <div className="seeker-layout">
+    <div className={`seeker-layout${isJobDetailPage ? ' seeker-layout--job-detail' : ''}`}>
       <div className="seeker-layout__shell">
         <DashboardTopbar role="seeker" userName={user?.firstName} onLogout={logout} />
         <main className="seeker-layout__main">
